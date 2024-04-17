715ad4aba938d798fa78dc4e2b5dfb47e7530316
trunghoangnt2003 <120396322+trunghoangnt2003@users.noreply.github.com>
2024-04-17 09:40:41 +0700

    1

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ function openApi(app: INestApplication<any>) {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors()
   openApi(app);
   await app.listen(3000);
 }
