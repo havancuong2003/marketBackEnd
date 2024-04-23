9ec7e38639a49ecdd1305454eed1663ecf02f105
ManhHuy15 <huya3k52@gmail.com>
2024-04-23 11:22:06 +0700

    lastest

--- src/account/dto/register-account.dto.ts
+++ src/account/dto/register-account.dto.ts
@@ -1,9 +1,11 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateAccountDto } from "./create-account.dto";
-import { IsNotEmpty, IsString } from "class-validator";
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateAccountDto } from './create-account.dto';
+import { IsEmail, IsEmpty, IsNotEmpty, IsString } from 'class-validator';
 
 export class RegisterAccountDto extends PartialType(CreateAccountDto) {
-    @IsNotEmpty()
-    @IsString()
-    username: string;
- }
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+  @IsEmpty()
+  balance: number;
+}
