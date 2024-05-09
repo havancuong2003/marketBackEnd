a4b10b3261f2b7fb8e0ba1d369c6c7436dd7f129
havancuong2003 <havancuong07122003@gmail.com>
2024-05-09 16:16:23 +0700

    add: add findbyemail

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -18,6 +18,12 @@ export class AccountService implements IAccountService {
     @InjectRepository(Account)
     private readonly accountRepository: Repository<Account>,
   ) {}
+  findByEmailRegister(email: string) {
+    throw new Error('Method not implemented.');
+  }
+  findByUserNameRegister(email: string) {
+    throw new Error('Method not implemented.');
+  }
   findAll() {
     return this.accountRepository.find();
   }
@@ -27,46 +33,46 @@ export class AccountService implements IAccountService {
     return user;
   }
   async findByEmail(email: string) {
-    const user =  await this.accountRepository.findOneBy({ email: email });
+    const user = await this.accountRepository.findOneBy({ email: email });
     if (!user) throw new Error('user not found');
     return user;
   }
-  async updatePassWord(id: UUID, password: string, currentPassword:string) {
+  async updatePassWord(id: UUID, password: string, currentPassword: string) {
     const saltOrRounds = 10;
 
     const user = await this.accountRepository.findOneBy({ id: id });
-    const currentPasswordHash = await bcrypt.hash(currentPassword, saltOrRounds);
+    const currentPasswordHash = await bcrypt.hash(
+      currentPassword,
+      saltOrRounds,
+    );
 
-    if(!await bcrypt.compare(currentPassword, user.password)){
+    if (!(await bcrypt.compare(currentPassword, user.password))) {
       throw new BadRequestException('current password not match');
     }
-    
-    const passwordHash =  await bcrypt.hash(password, saltOrRounds);
+
+    const passwordHash = await bcrypt.hash(password, saltOrRounds);
     return this.accountRepository.update(id, { password: passwordHash });
   }
   async update(id: UUID, updateUserDto: UpdateAccountDto) {
     return this.accountRepository.update(id, updateUserDto);
   }
-  async create(create:RegisterAccountDto){
-     return await this.accountRepository.create(create);
+  async create(create: RegisterAccountDto) {
+    return await this.accountRepository.create(create);
   }
-  async save(account:Account){
+  async save(account: Account) {
     return await this.accountRepository.save(account);
   }
-  informationAccount(id:UUID){
+  informationAccount(id: UUID) {
     return this.accountRepository.findOneBy({ id: id });
   }
-  async updateUserName(id:UUID,username:string){
+  async updateUserName(id: UUID, username: string) {
     const user = await this.accountRepository.findOneBy({ username: username });
-    if(user){
+    if (user) {
       throw new BadRequestException('username already exist');
     }
-    return this.accountRepository.update(id,{username:username});
-
+    return this.accountRepository.update(id, { username: username });
   }
-  async updateAvatar(id:UUID,avatar:string){
-    return await this.accountRepository.update(id,{avatar:avatar});
+  async updateAvatar(id: UUID, avatar: string) {
+    return await this.accountRepository.update(id, { avatar: avatar });
   }
-
-  
 }
